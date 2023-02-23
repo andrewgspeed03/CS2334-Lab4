7f7811ce9f44783326009f2179e66f427856c131
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-02-22 21:25:35 -0600

    Add files via upload
    Implemented all methods needed in both Playlist and Song to pass the test.

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,5 +1,80 @@
+import java.util.Arrays;
 
+/**
+ * 
+ * @author good0161
+ * @version 4.0.2
+ *
+ */
 public class Playlist {
-
-
+	private Song[] songs;
+	private int numSongs;
+	private static final int MIN_CAPACITY = 3;
+	
+	public Playlist() {
+		this.songs = new Song[MIN_CAPACITY];
+		this.numSongs = 0;
+	}
+	public Playlist(int capacity) {
+		if(capacity >= MIN_CAPACITY)
+			this.songs = new Song[capacity];
+		else
+			this.songs = new Song[MIN_CAPACITY];
+		this.numSongs = 0;
+	}
+	
+	public int getCapacity() {
+		return songs.length;
+	}
+	public int getNumSongs() {
+		return numSongs;
+	}
+	public Song getSong(int index) {
+		if(index > -1 && index < numSongs)
+			return songs[index];
+		else
+			return null;
+	}
+	public Song[] getSongs() {
+		return Arrays.copyOf(songs, numSongs);
+	}
+	
+	public boolean addSong(Song song) {
+		return addSong(numSongs, song);
+	}
+	public boolean addSong(int index, Song song) {
+		if(index > -1 && index <= numSongs && song != null && numSongs != songs.length) {
+			for(int i = numSongs - 1; i >= index; i--)
+				songs[i+1] = songs[i];
+			songs[index] = song;
+			numSongs++;
+			return true;
+		}
+		else
+			return false;
+	}
+	public int addSongs(Playlist playlist) {
+		if (playlist == null)
+			return 0;
+		int y = playlist.numSongs;
+		int count = 0;
+		if ((count < y) && getCapacity() > numSongs ) 
+			for (int i = 0; (count < y) && (getCapacity() > getNumSongs()); ++i) {
+				addSong(playlist.getSong(i));
+				count++;
+			}
+		return count;
+	}
+	public Song removeSong() {
+		return removeSong(numSongs - 1);
+	}
+	public Song removeSong(int index) {
+		if (songs == null || index > numSongs - 1 || index < 0 || numSongs == 0) 
+			return null;
+		Song rtn = songs[index];
+		for (int i = index; i < numSongs -1; ++i) 
+			songs[i] = songs[i + 1];
+		numSongs--;
+		return rtn;
+	}
 }
--- src/Song.java
+++ src/Song.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
 
+/**
+ * 
+ * @author good0161
+ * @version 4.0.1
+ *
+ */
 public class Song {
-
-
+	private String title;
+	private String artist;
+	private int[] time;
+	
+	public Song(String title, String artist, int[] time) {
+		this.title = title;
+		this.artist = artist;
+		this.time = Arrays.copyOf(time, time.length);
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public int[] getTime() {
+		int[] copy = Arrays.copyOf(time, time.length);
+		return copy;
+	}
 }
